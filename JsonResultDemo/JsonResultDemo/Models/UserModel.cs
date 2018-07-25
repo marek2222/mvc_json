4c65cfa577494d232c16cf97532309e641081034
marek2222 <marekmusial2@o2.pl>
2018-07-25 21:43:20 +0200

    JsonResultDemo: 1.03:  Scenario 2: Get the list of users in JSON Format:
    Step 1: Add a class file “UserModel.cs” like the following.

---
+++ JsonResultDemo/JsonResultDemo/Models/UserModel.cs
@@ -0,0 +1,10 @@
+﻿namespace JsonResultDemo.Models
+{
+  public class UserModel
+  {
+    public int UserId { get; set; }
+    public string UserName { get; set; }
+    public string Company { get; set; }
+
+  }
+}
