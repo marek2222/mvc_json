72def9aabd73609f6b79c5c1599e56abe5a47648
marek2222 <marekmusial2@o2.pl>
2018-07-25 20:42:23 +0200

    Dr_SongLi: 1.01: Utworzono nowy projekt

---
+++ Dr.Song_Li/Dr.Song_Li-JSON_objects_in_jQuery_and_MVC/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Dr.Song_Li_JSON_objects_in_jQuery_and_MVC
+{
+  public class FilterConfig
+  {
+    public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+    {
+      filters.Add(new HandleErrorAttribute());
+    }
+  }
+}
---
+++ Dr.Song_Li/Dr.Song_Li-JSON_objects_in_jQuery_and_MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Dr.Song_Li_JSON_objects_in_jQuery_and_MVC.MvcApplication" Language="C#" %>
