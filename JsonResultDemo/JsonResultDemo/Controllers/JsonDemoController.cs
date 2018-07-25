f34d13d64fce4c94da36bf4d4a8cd10c49cf8299
marek2222 <marekmusial2@o2.pl>
2018-07-25 21:39:01 +0200

    JsonResultDemo: 1.02:  Scenario 1: Send JSON Content welcome note based on user type

---
+++ JsonResultDemo/JsonResultDemo/Controllers/JsonDemoController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JsonResultDemo.Controllers
+{
+  public class JsonDemoController : Controller
+  {
+
+    /// <summary>   
+    /// Welcome Note Message   
+    /// </summary>   
+    /// <returns>In a Json Format</returns>   
+    public JsonResult WelcomeNote()
+    {
+      bool isAdmin = false;
+      //TODO: Check the user if it is admin or normal user, (true-Admin, false- Normal user)   
+      string output = isAdmin ? "Welcome to the Admin User" : "Welcome to the User";
+
+      return Json(output, JsonRequestBehavior.AllowGet);
+    }
+
+  }
+}
