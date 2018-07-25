79affa87507183b8ec19de15e4f31bfb07c2d72e
marek2222 <marekmusial2@o2.pl>
2018-07-25 21:45:16 +0200

    JsonResultDemo: 1.04:  Scenario 2: Get the list of users in JSON Format:
    Step 3: Add one method named GetUsers in the JsonDemoController.cs file that will return the list of sample users.

--- JsonResultDemo/JsonResultDemo/Controllers/JsonDemoController.cs
+++ JsonResultDemo/JsonResultDemo/Controllers/JsonDemoController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using JsonResultDemo.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -21,6 +22,38 @@ namespace JsonResultDemo.Controllers
 
       return Json(output, JsonRequestBehavior.AllowGet);
     }
+
+    /// <summary>   
+    /// Get the Users   
+    /// </summary>   
+    /// <returns></returns>   
+    private List<UserModel> GetUsers()
+    {
+      var usersList = new List<UserModel>
+            {
+                new UserModel
+                {
+                    UserId = 1,
+                    UserName = "Ram",
+                    Company = "Mindfire Solutions"
+                },
+                new UserModel
+                {
+                    UserId = 1,
+                    UserName = "chand",
+                    Company = "Mindfire Solutions"
+                },
+                new UserModel
+                {
+                    UserId = 1,
+                    UserName = "Abc",
+                    Company = "Abc Solutions"
+                }
+            };
+
+      return usersList;
+    }
+
 
   }
 }
