d6d9d2287cb3be1ff81c57129c24fa3edbeaa915
marek2222 <marekmusial2@o2.pl>
2018-07-26 11:45:23 +0200

    JsonResultDemo: 1.09:  Unit Testing the JsonResult in MVC - tests

---
+++ JsonResultDemo/JsonResultDemo.Tests/Controllers/JsonDemoControllerTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using JsonResultDemo.Controllers;
+using System.Web.Mvc;
+
+namespace WebApplication1.Tests.Controllers
+{
+  [TestClass]
+  public class JsonDemoControllerTest
+  {
+    [TestMethod]
+    public void WelcomeNote()
+    {
+      JsonDemoController controller = new JsonDemoController();
+
+      JsonResult result = controller.WelcomeNote();
+      string msg = Convert.ToString(result.Data);
+      // Assert   
+      Assert.AreEqual("Welcome to the User", msg);
+    }
+
+  }
+}
