23d29d89e19eca706fb96820b55f5a6f5db7363c
marek2222 <marekmusial2@o2.pl>
2018-07-26 10:46:35 +0200

    JsonResultDemo: 1.08:  Scenario 4: How to handle huge amount of JSON Data

--- JsonResultDemo/JsonResultDemo/Controllers/JsonDemoController.cs
+++ JsonResultDemo/JsonResultDemo/Controllers/JsonDemoController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Script.Serialization;
@@ -46,17 +47,69 @@ namespace JsonResultDemo.Controllers
     }
 
 
+    // Scenario 3 : Create JSON data at the client side and send content to the controller
     public ActionResult Sample()
     {
       return View();
     }
 
     [HttpPost]
-    public JsonResult UpdateUserDetail(string usersJson) {
+    public JsonResult UpdateUserDetail(string usersJson)
+    {
       var js = new JavaScriptSerializer();
       UserModel[] user = js.Deserialize<UserModel[]>(usersJson);
       return Json("User Details are updated");
     }
+
+
+
+    /// Scenario 4: How to handle huge amount of JSON Data
+    private List<UserModel> GetUsersHugeData()
+    {
+      var userList = new List<UserModel>();
+      UserModel user;
+      for (int i = 1; i < 51000; i++)
+      {
+        user = new UserModel
+        {
+          UserId = i,
+          UserName = "User-" + i.ToString(),
+          Company = "Company-" + i.ToString()
+        };
+        userList.Add(user);
+      }
+      return userList;
+    }
+
+
+    public JsonResult GetUsersHugeList()
+    {
+      // Default JsonData depth is 100.
+      // Then appears a error screen because count of data is mere then 100
+      var users = GetUsersHugeData();
+      return Json(users, JsonRequestBehavior.AllowGet);
+    }
+
+    /// <summary>   
+    /// Override the JSON Result with Max integer JSON lenght   
+    /// </summary>   
+    /// <param name="data">Data</param>   
+    /// <param name="contentType">Content Type</param>   
+    /// <param name="contentEncoding">Content Encoding</param>   
+    /// <param name="behavior">Behavior</param>   
+    /// <returns>As JsonResult</returns>   
+    protected override JsonResult Json(object data, string contentType, Encoding contentEncoding, JsonRequestBehavior behavior)
+    {
+      return new JsonResult()
+      {
+        Data                = data,
+        ContentType         = contentType,
+        ContentEncoding     = contentEncoding,
+        JsonRequestBehavior = behavior,
+        MaxJsonLength       = Int32.MaxValue
+      };
+    }
+
 
   }
 }
